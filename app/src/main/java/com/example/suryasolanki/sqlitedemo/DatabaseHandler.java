99e89c0e0f1c8ba64e201a4bff1db0df3457a5f5
surya.solanki <keithgeorge095@gmail.com>
2017-12-13 20:03:09 +0530

    DatabaseHandler Class

--- app/src/main/java/com/example/suryasolanki/sqlitedemo/DatabaseHandler.java
+++ app/src/main/java/com/example/suryasolanki/sqlitedemo/DatabaseHandler.java
@@ -1,8 +1,16 @@
 package com.example.suryasolanki.sqlitedemo;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by surya.solanki on 12/13/2017.
@@ -25,7 +33,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
-        String Create_Contacts_table="CREATE TABLE" + TABLE_CONTACTS + "( " +
+        String Create_Contacts_table="CREATE TABLE " + TABLE_CONTACTS + " ( " +
                 Key_Id + " INTEGER, " + Key_Name + " TEXT, " +
                 Key_PHONE_NO + " TEXT )";
 
@@ -38,5 +46,60 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
 
         onCreate(sqLiteDatabase);
+    }
+
+
+    public void addContact(Contact contact){
+       SQLiteDatabase db=this.getWritableDatabase();
+
+        ContentValues values=new ContentValues();
+        values.put(Key_Name,contact.getName());
+        values.put(Key_PHONE_NO,contact.getPhoneNumber());
+
+        db.insert(TABLE_CONTACTS,null,values);
+        db.close();
+    }
+
+    public Contact getContact(int id){
+
+        return new Contact( );
+    }
+
+    public void deleteContact(int id){
+        SQLiteDatabase db=this.getWritableDatabase();
+        String new_id[]=new String[] {String.valueOf(id)};
+        Log.d("DELETE","DELETING....");
+//        String DELETE_QUERY="DELETE FROM  "+ TABLE_CONTACTS + " WHERE " + Key_Id + " = "+ id;
+//        db.execSQL(DELETE_QUERY);
+
+        db.delete(TABLE_CONTACTS,Key_Id + " = ? ",new_id);
+        Log.d("DELETE","DELETED....");
+        db.close();
+
+    }
+
+    public List<Contact> getAllContact(){
+        List<Contact> contactList= new ArrayList<>();
+
+        String SELECT_QUERY="SELECT * FROM "+ TABLE_CONTACTS;
+
+        SQLiteDatabase db=this.getReadableDatabase();
+        Cursor cursor=db.rawQuery(SELECT_QUERY,null);
+
+        if(cursor.moveToFirst()){
+            do{
+                Contact contact=new Contact();
+               // contact.setId(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setPhoneNumber(cursor.getString(2));
+
+                contactList.add(contact);
+            }while(cursor.moveToNext());
+        }
+        return contactList;
+    }
+
+    public void deleteContact(){
+
     }
 }
--- app/src/main/java/com/example/suryasolanki/sqlitedemo/MainActivity.java
+++ app/src/main/java/com/example/suryasolanki/sqlitedemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.suryasolanki.sqlitedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler db=new DatabaseHandler(this);
+
+        db.deleteContact(0);
+
+        Log.d("Insert: ", "Inserting....");
+
+        db.addContact(new Contact("Surya Pratap","9929856749"));
+        db.addContact(new Contact("Ravi Saraswat","8952979627"));
+        db.addContact(new Contact("Rishabh Rana","9166626970"));
+        db.addContact(new Contact("Shashank Gupta","99501994492"));
+
+
+
+        Log.d("Reading", "Reading Data...");
+
+        List<Contact> contacts=db.getAllContact();
+
+        for(Contact cn:contacts){
+            String log="ID: "+ cn.getId() + " Name: " + cn.getName() + " Phone: " + cn.getPhoneNumber();
+
+            Log.d("Name: ", log);
+        }
+
     }
 }
